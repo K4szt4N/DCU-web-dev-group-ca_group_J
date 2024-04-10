2c7757609e76c8be8e2579bc8e02dc08526f0878
William <william.mallevays@epitech.eu>
2024-04-10 20:07:20 +0100

    Add: dashbaord layout

--- front/scripts/script.js
+++ front/scripts/script.js
@@ -151,3 +151,49 @@ function drawChart6() {
 
   chart.draw(data, google.charts.Bar.convertOptions(options));
 };
+
+function drawChart7() {
+  var data = google.visualization.arrayToDataTable([
+    ['City', '2010 Population', '2000 Population'],
+    ['New York City, NY', 8175000, 8008000],
+    ['Los Angeles, CA', 3792000, 3694000],
+    ['Chicago, IL', 2695000, 2896000],
+    ['Houston, TX', 2099000, 1953000],
+    ['Philadelphia, PA', 1526000, 1517000]
+  ]);
+
+  var options = {
+    title: 'Population of Largest U.S. Cities',
+    chartArea: { width: '50%' },
+    hAxis: {
+      title: 'Total Population',
+      minValue: 0,
+    },
+    vAxis: {
+      title: 'City'
+    }
+  };
+
+  var chart = new google.visualization.BarChart(document.getElementById('barchart_values'));
+  chart.draw(data, options);
+}
+
+function drawChart8() {
+  var data = google.visualization.arrayToDataTable([
+    ['Year', 'Sales', 'Expenses'],
+    ['2013',  1000,      400],
+    ['2014',  1170,      460],
+    ['2015',  660,       1120],
+    ['2016',  1030,      540]
+  ]);
+
+  var options = {
+    title: 'Company Performance',
+    curveType: 'function',
+    legend: { position: 'bottom' }
+  };
+
+  var chart = new google.visualization.LineChart(document.getElementById('curve_chart'));
+
+  chart.draw(data, options);
+}
